2c1b70316a0d988cfb2573643461d5d260d748ae
marsrian <marsrian40@gmail.com>
2022-05-27 02:32:52 +0600

    null vs undefined cleared

---
+++ null-vs-undefined.js
@@ -0,0 +1,23 @@
+let Rian;
+console.log(Rian);
+
+function add(num1, num2) {
+    console.log(num1 + num2);
+}
+const sum = add(34, 56);
+console.log(sum);
+
+function added(num1, num2) {
+    console.log(num1, num2);
+}
+const result = added(13);
+console.log(result);
+
+const batsman = { name: "Rahul", phone: 17956, }
+console.log(batsman.familyBacground);
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [13, 15, 14]
+console.log(ages[10]);
--- true-false.js
+++ true-false.js
@@ -10,7 +10,7 @@
 // '0', ' ', [], {}, "false" 
 
 // const name = "0";
-let name = 12;
+let name = 0;
 if (name || name == 0) {
     console.log("condition is true");
 } else {
