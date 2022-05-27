1d60cea29c78f9a94bd4beebd36c13dc28dcebb5
marsrian <marsrian40@gmail.com>
2022-05-28 00:26:47 +0600

    scope-practise

---
+++ scope.js
@@ -0,0 +1,22 @@
+let bonus = 30;
+
+function sum(first, second) {
+    let result = first + second + bonus;
+    // console.log(result);
+    // console.log(bonus);
+    if (result > 10) {
+        let mood = "happy";
+        mood = "fishy";
+        mood = "funky";
+        mood = "cranky"
+        console.log(mood);
+    }
+    console.log(day);
+    var day = "friday"
+        // console.log(day);
+
+    return result;
+}
+const output = sum(12, 23);
+// console.log(bonus);
+console.log(output);
