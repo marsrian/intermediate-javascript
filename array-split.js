69611fb7ab5cdaa9b6c8b32f3d54e9a2de1329a7
marsrian <marsrian40@gmail.com>
2022-05-28 01:29:57 +0600

    array-splice-slice-join-practise

---
+++ array-split.js
@@ -0,0 +1,13 @@
+const nums = [4, 6, 8, 7, 9, 12, 34, 45];
+// Slice Practice:
+// const part = nums.slice(2, 5);
+
+// Splice p[ractice:
+const removed = nums.splice(3, 4, 40);
+
+// console.log(part);
+// console.log(removed);
+// console.log(nums);
+
+const together = nums.join(",");
+console.log(together);
