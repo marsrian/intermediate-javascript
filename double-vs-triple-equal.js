ff7dd81875fc556397e605470bcdb11802eccd4b
marsrian <marsrian40@gmail.com>
2022-05-27 10:55:28 +0600

    double-vs-triple-equal

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,34 @@
+// d0uble equal : value same 
+const first = 2;
+const second = "2";
+if (first == second) {
+    console.log("condition is true");
+} else {
+    console.log("condition is false");
+}
+// double equal e 1 and true value same
+const age = 1;
+const age2 = true;
+if (age == age2) {
+    console.log("condition is correct");
+} else {
+    console.log("condition is not correct");
+}
+
+// double equal e 0 and false value same
+const tkPoisa = 0;
+const tkPoisa2 = false;
+if (tkPoisa == tkPoisa2) {
+    console.log("condition is correct");
+} else {
+    console.log("condition is not correct");
+}
+
+// triple equal : type and value same
+const friend1 = 3;
+const friend2 = "3";
+if (friend1 === friend2) {
+    console.log("condition is perfect");
+} else {
+    console.log("condition is non perfect");
+}
